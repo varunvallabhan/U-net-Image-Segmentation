c530cc570a27fb5e9a72a3d9990eed1a69849992
Varun <vvvallab@amazon.com>
2021-09-30 10:33:34 -0400

    Make file export

--- TestApp.java
+++ TestApp.java
@@ -1,5 +1,10 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
+import java.io.IOException;
+
+public void whenWriteStringUsingBufferedWritter_thenCorrect() 
+  throws IOException {
+    String str = "Hello";
+    BufferedWriter writer = new BufferedWriter(new FileWriter(test.txt));
+    writer.write("Hello Hello");
+    
+    writer.close();
 }
